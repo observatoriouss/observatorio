c5844ffaef93c9a948336338faa42c87d4d1f742
josemr22 <josemr.99@outlook.com>
2024-04-21 13:17:20 -0500

    test env in next config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,10 @@
 const nextConfig = {
     images: {
         domains: ['lightsoft.blob.core.windows.net']
-    }
+    },
+    env: {
+        API_URL: process.env.API_URL,
+    },
 }
 
 module.exports = nextConfig
--- src/config/api.ts
+++ src/config/api.ts
@@ -1,4 +1,4 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const API_URL = process.env.API_URL || '';
+export const API_URL = process.env.API_URL || 'https://observatorio-uss.azurewebsites.net/api';
