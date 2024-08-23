10fa8e86fee13d0ec588ed8842387a1a0c34b060
Renatto Farid <renattofaridper@gmail.com>
2024-08-23 13:10:00 -0500

    feat: Add raw.githubusercontent.com to image domains in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['lightsoft.blob.core.windows.net', 'storageuss.blob.core.windows.net']
+        domains: ['lightsoft.blob.core.windows.net', 'storageuss.blob.core.windows.net', 'raw.githubusercontent.com']
     }
 }
 
