2dcc5dea6a640a03ce22ef4a41306b97e5c2639e
jamesllalle-lgtm <james.llalle@uss.edu.pe>
2026-04-09 16:14:47 -0500

    correcion errores push

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'standalone', // <--- Añadimos esto para optimizarlo en Azure
     images: {
         domains: ['lightsoft.blob.core.windows.net', 'storageuss.blob.core.windows.net', 'raw.githubusercontent.com', 'cdn.discordapp.com']
     }
